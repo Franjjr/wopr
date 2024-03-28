3c1c63f2c588cfcc213683d6978d99a723d1dce4
Mercedes0708 <144907375+Mercedes0708@users.noreply.github.com>
2024-03-28 20:46:47 +0000

    feat: landing, faltan los iconos e imagenes

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -1,6 +1,7 @@
 import React from "react";  // Import react into the bundle
 import ReactDOM from "react-dom";
 import "../styles/index.css";  // Include your index.scss file into the bundle
+import ".././styles/home.css";
 import Layout from "./Layout.jsx";  // Import your own components
 
 //render your react application
