5b320efe35b128d3de388485dd4ed80f4ac4b256
Fran Julian <138630369+Franjjr@users.noreply.github.com>
2024-04-06 09:21:49 +0000

    FEAT: Limpieza de Warnings

--- src/front/js/pages/ManufacturingOrders.jsx
+++ src/front/js/pages/ManufacturingOrders.jsx
@@ -12,18 +12,18 @@ export const ManufacturingOrders = () => {
 <div id="page-top"> 
 { !store.isLogin ? <h1>FORBIDEN</h1>:
             <div className="container-fluid">
-            <h1 class="h3 mb-2 text-gray-800">Manufacturing Orders <i class="fa-regular fa-paper-plane"></i> </h1>
-    <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below.
+            <h1 className="h3 mb-2 text-gray-800">Manufacturing Orders <i className="fa-regular fa-paper-plane"></i> </h1>
+    <p className="mb-4">DataTables is a third party plugin that is used to generate the demo table below.
         For more information about DataTables, please visit the <a target="_blank"
             href="https://datatables.net">official DataTables documentation</a>.</p>
     
-    <div class="card shadow mb-4">
-        <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Orders</h6>
+    <div className="card shadow mb-4">
+        <div className="card-header py-3">
+            <h6 className="m-0 font-weight-bold text-primary">Orders</h6>
         </div>
-        <div class="card-body">
-            <div class="table-responsive">
-                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+        <div className="card-body">
+            <div className="table-responsive">
+                <table className="table table-bordered" id="dataTable" width="100%" cellSpacing="0">
                     <thead>
                         <tr>
                             <th>Recipe to</th>
